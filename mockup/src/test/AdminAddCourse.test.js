d0efced9fea0590f724a94786710ad1dba8b0c94
Thanawat Lukuan <thanawat_lukuan@cmu.ac.th>
2018-04-28 23:09:42 +0700

    Add Course not Successfully

--- mockup/src/test/AdminAddCourse.test.js
+++ mockup/src/test/AdminAddCourse.test.js
@@ -9,6 +9,10 @@ import AdminAddCourse from '../Views/AdminAddCourse'
 describe('Test Add Course',()=>{
   const stubs=[
     {id : '953234', name : 'Computer Programming', lecturers : 'Dr.Prompong Sugunasil', seats : '30',
+    credits : '3', days : 'Monday', timefrom : '9:30', timeto : '10:00', semester : '1/2560'},
+    {id : '', name : 'Computer Programming', lecturers : 'Dr.Prompong Sugunasil', seats : '30',
+    credits : '3', days : 'Monday', timefrom : '9:30', timeto : '10:00', semester : '1/2560'},
+    {id : '953321', name : 'Software Construct Testing and Maintenance', lecturers : '', seats : '30',
     credits : '3', days : 'Monday', timefrom : '9:30', timeto : '10:00', semester : '1/2560'}
   ]
   it('Add Course renders correctly',()=>{
@@ -30,6 +34,36 @@ describe('Test Add Course',()=>{
     expect(wrapper.state('id')).toBe('953234')
     expect(wrapper.state('name')).toBe('Computer Programming')
     expect(wrapper.state('lecturers')).toBe('Dr.Prompong Sugunasil')
+    expect(wrapper.state('seats')).toBe('30')
+    expect(wrapper.state('credits')).toBe('3')
+    expect(wrapper.state('days')).toBe('Monday')
+    expect(wrapper.state('timefrom')).toBe('9:30')
+    expect(wrapper.state('timeto')).toBe('10:00')
+    expect(wrapper.state('semester')).toBe('1/2560')
+    expect(wrapper.instance().handleSubmit()).toBeCalled
+  })
+  it('Add Course not Successfully',()=>{
+    const wrapper = shallow(<AdminAddCourse/>)
+    wrapper.setState(stubs[1])
+    wrapper.instance().handleSubmit()
+    expect(wrapper.state('id')).toBe('')
+    expect(wrapper.state('name')).toBe('Computer Programming')
+    expect(wrapper.state('lecturers')).toBe('Dr.Prompong Sugunasil')
+    expect(wrapper.state('seats')).toBe('30')
+    expect(wrapper.state('credits')).toBe('3')
+    expect(wrapper.state('days')).toBe('Monday')
+    expect(wrapper.state('timefrom')).toBe('9:30')
+    expect(wrapper.state('timeto')).toBe('10:00')
+    expect(wrapper.state('semester')).toBe('1/2560')
+    expect(wrapper.instance().handleSubmit()).toBeCalled
+  })
+  it('Add Course not Successfully',()=>{
+    const wrapper = shallow(<AdminAddCourse/>)
+    wrapper.setState(stubs[2])
+    wrapper.instance().handleSubmit()
+    expect(wrapper.state('id')).toBe('953321')
+    expect(wrapper.state('name')).toBe('Software Construct Testing and Maintenance')
+    expect(wrapper.state('lecturers')).toBe('')
     expect(wrapper.state('seats')).toBe('30')
     expect(wrapper.state('credits')).toBe('3')
     expect(wrapper.state('days')).toBe('Monday')
