0079950e31d878c21c29dad4cad8847ff7da11fc
Thapakorn Tuwaemuesa <thapakorn604@gmail.com>
2018-04-28 15:04:52 +0700

    Add login.test

--- mockup/src/test/Login.test.js
+++ mockup/src/test/Login.test.js
@@ -7,8 +7,72 @@ Enzyme.configure({ adapter: new Adapter() })
 
 import Login from '../Views/Login'
 
-it('renders correctly', () => {
-  const wrapper = shallow(<Login />)
+describe('Test login', () => {
+    const stubs = [
+        {account : 'student',password :'student',type : 'Student'},
+        {account : 'lecturer',password :'lecturer',type : 'Lecturer'},
+        {account : 'lecturer',password :'student',type : 'Student'},
+        {account : 'lecturer',password :'student',type : 'Lecturer'},
+        {account : 'lecturer',password :'lecturer',type : 'Student'}
+    ]
+  it('should renders correctly', () => {
+    const wrapper = shallow(<Login />)
+    expect(wrapper.state('type')).toBe('Student')
+    expect(wrapper.state('account')).toBe('')
+    expect(wrapper.state('password')).toBe('')
+    })
+ 
+ it('should login correctly by Student', () => {
+   const wrapper = shallow(<Login />)
+    wrapper.setState(stubs[0])
+    wrapper.instance().handleSignIn()
+    expect(wrapper.state('account')).toBe('student')
+    expect(wrapper.state('password')).toBe('student')
+    expect(wrapper.state('type')).toBe('Student')
+    expect(wrapper.instance().handleSignIn()).toBeCalled
+ })
 
-  expect(wrapper.state('type')).toBe('Student')
+ it('should login correctly by Lecturer', () => {
+   const wrapper = shallow(<Login />)
+    wrapper.setState(stubs[1])
+    wrapper.instance().handleSignIn()
+    expect(wrapper.state('account')).toBe('lecturer')
+    expect(wrapper.state('password')).toBe('lecturer')
+    expect(wrapper.state('type')).toBe('Lecturer')
+    expect(wrapper.instance().handleSignIn()).toBeCalled
+ })
+
+ it('should not login by wrong account', () => {
+   const wrapper = shallow(<Login />)
+    wrapper.setState(stubs[2])
+    wrapper.instance().handleSignIn()
+    expect(wrapper.state('account')).toBe('lecturer')
+    expect(wrapper.state('password')).toBe('student')
+    expect(wrapper.state('type')).toBe('Student')
+    expect(wrapper.instance().handleSignIn()).toBeCalled
+ })
+
+ it('should not login by wrong password', () => {
+   const wrapper = shallow(<Login />)
+    wrapper.setState(stubs[3])
+    wrapper.instance().handleSignIn()
+    expect(wrapper.state('account')).toBe('lecturer')
+    expect(wrapper.state('password')).toBe('student')
+    expect(wrapper.state('type')).toBe('Lecturer')
+    expect(wrapper.instance().handleSignIn()).toBeCalled
+ })
+
+ it('should not login by wrong type', () => {
+   const wrapper = shallow(<Login />)
+    wrapper.setState(stubs[4])
+    wrapper.instance().handleSignIn()
+    expect(wrapper.state('account')).toBe('lecturer')
+    expect(wrapper.state('password')).toBe('lecturer')
+    expect(wrapper.state('type')).toBe('Student')
+    expect(wrapper.instance().handleSignIn()).toBeCalled
+ })
+    
 })
+
+
+
