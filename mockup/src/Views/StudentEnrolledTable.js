603fe70ad2265b85eca57684ba5d5c168c696012
qliphothparadox <de_wang@cmu.ac.th>
2018-04-29 14:07:37 +0700

    update withdraw
    update withdraw function. test not finish yet

--- mockup/src/Views/StudentEnrolledTable.js
+++ mockup/src/Views/StudentEnrolledTable.js
@@ -4,24 +4,85 @@ import {Button} from 'reactbulma'
 class StudentEnrolledTable extends Component {
     constructor(props) {
         super(props);
-        this.state = { 
-            allcredit : '19'
+        this.state = {
+            allcredit : 19
          }
-
-        this.handleWithdraw = this.handleWithdraw.bind(this)
+        this.handleWithdraw1 = this.handleWithdraw1.bind(this)
+        this.handleWithdraw2 = this.handleWithdraw2.bind(this)
+        this.handleWithdraw3 = this.handleWithdraw3.bind(this)
+        this.handleWithdraw4 = this.handleWithdraw4.bind(this)
+        this.handleWithdraw5 = this.handleWithdraw5.bind(this)
+        this.handleWithdraw6 = this.handleWithdraw6.bind(this)
+        this.handleWithdraw7 = this.handleWithdraw7.bind(this)
+    }
+    handleWithdraw1(){
+        if(window.confirm('Are you sure to withdraw this course ?')==true)
+    {
+            document.getElementById("c1").hidden="true"
+            this.setState ({
+                allcredit :this.state.allcredit-3
+            })
+    }
+    }
+    handleWithdraw2(){
+        if(window.confirm('Are you sure to withdraw this course ?')==true)
+    {
+            document.getElementById("c2").hidden="true"
+            this.setState ({
+                allcredit :this.state.allcredit-3
+            })
+    }
+    }
+    handleWithdraw3(){
+        if(window.confirm('Are you sure to withdraw this course ?')==true)
+    {
+            document.getElementById("c3").hidden="true"
+            this.setState ({
+                allcredit :this.state.allcredit-3
+            })
+    }
+    }
+    handleWithdraw4(){
+        if(window.confirm('Are you sure to withdraw this course ?')==true)
+    {
+            document.getElementById("c4").hidden="true"
+            this.setState ({
+                allcredit :this.state.allcredit-3
+            })
+    }
     }
 
-    handleWithdraw(){
+    handleWithdraw5(){
+        if(window.confirm('Are you sure to withdraw this course ?')==true)
+    {
+            document.getElementById("c5").hidden="true"
+            this.setState ({
+                allcredit :this.state.allcredit-3
+            })
+    }
+    }
+    handleWithdraw7(){
         if(window.confirm('Are you sure to withdraw this course ?')==true)
 		{
             document.getElementById("c7").hidden="true"
             this.setState ({
-                allcredit : '18'
+                allcredit : this.state.allcredit-1
             })
 		}
     }
-    render() { 
-        return ( 
+
+    handleWithdraw6(){
+        if(window.confirm('Are you sure to withdraw this course ?')==true)
+		{
+            document.getElementById("c6").hidden="true"
+            this.setState ({
+                allcredit :this.state.allcredit-3
+            })
+		}
+    }
+
+    render() {
+        return (
             <table className="table">
                     <thead>
                         <tr>
@@ -35,7 +96,7 @@ class StudentEnrolledTable extends Component {
                             <th></th>
                         </tr>
                     </thead>
-                        <tr>
+                        <tr id="c1">
                             <td>1</td>
                             <td>953213</td>
                             <td>Operating System Principle and LANG</td>
@@ -43,9 +104,9 @@ class StudentEnrolledTable extends Component {
                             <td>TBA</td>
                             <td>Dr.Passakorn Pannachitta</td>
                             <td>3</td>
-                            <td><Button danger>Withdraw</Button></td>
-                        </tr>  
-                        <tr>
+                            <td><Button danger onClick={this.handleWithdraw1}>Withdraw</Button></td>
+                        </tr>
+                        <tr id="c2">
                             <td>2</td>
                             <td>208255</td>
                             <td>Math for SE</td>
@@ -53,9 +114,9 @@ class StudentEnrolledTable extends Component {
                             <td>9.30 - 11.00</td>
                             <td>Lect.Morakot Intanon</td>
                             <td>3</td>
-                            <td><Button danger>Withdraw</Button></td>
+                            <td><Button danger onClick={this.handleWithdraw2}>Withdraw</Button></td>
                         </tr>
-                        <tr>
+                        <tr id="c3">
                             <td>3</td>
                             <td>953201</td>
                             <td>Algorithm Design and Analysis</td>
@@ -63,9 +124,9 @@ class StudentEnrolledTable extends Component {
                             <td>14.00 - 17.00</td>
                             <td>Dr.Pattama Longani</td>
                             <td>3</td>
-                            <td><Button danger>Withdraw</Button></td>
+                            <td><Button danger onClick={this.handleWithdraw3}>Withdraw</Button></td>
                         </tr>
-                        <tr>
+                        <tr id="c4">
                             <td>4</td>
                             <td>001202</td>
                             <td>English in Sceince context</td>
@@ -73,9 +134,9 @@ class StudentEnrolledTable extends Component {
                             <td>8.00 - 9.30</td>
                             <td>Pierre Yanofsky</td>
                             <td>3</td>
-                            <td><Button danger>Withdraw</Button></td>
+                            <td><Button danger onClick={this.handleWithdraw4}>Withdraw</Button></td>
                         </tr>
-                        <tr>
+                        <tr id="c5">
                             <td>5</td>
                             <td>207110</td>
                             <td>Physics:Scince that change the world</td>
@@ -83,9 +144,9 @@ class StudentEnrolledTable extends Component {
                             <td>9.30 - 11.00</td>
                             <td>Lect.Krit Sriphomsett, Lect.Prakarn Unachak</td>
                             <td>3</td>
-                            <td><Button danger>Withdraw</Button></td>
+                            <td><Button danger onClick={this.handleWithdraw5}>Withdraw</Button></td>
                         </tr>
-                        <tr>
+                        <tr id="c6">
                             <td>6</td>
                             <td>751100</td>
                             <td>Economics in Everyday life</td>
@@ -93,7 +154,7 @@ class StudentEnrolledTable extends Component {
                             <td>12.30 - 14.00</td>
                             <td>Lect.Rossasin Tangpolponsawasdi</td>
                             <td>3</td>
-                            <td><Button danger>Withdraw</Button></td>
+                            <td><Button danger onClick={this.handleWithdraw6}>Withdraw</Button></td>
                         </tr>
                         <tr id="c7">
                             <td>7</td>
@@ -103,8 +164,8 @@ class StudentEnrolledTable extends Component {
                             <td>17.00 - 18.00</td>
                             <td>Lect.Pakorn Tuisri</td>
                             <td>1</td>
-                            <td><Button danger onClick={this.handleWithdraw}>Withdraw</Button></td>
-                        </tr> 
+                            <td><Button danger onClick={this.handleWithdraw7}>Withdraw</Button></td>
+                        </tr>
                         <tr>
                             <th></th>
                             <th></th>
@@ -114,10 +175,10 @@ class StudentEnrolledTable extends Component {
                             <th>All credits</th>
                             <th>{this.state.allcredit}</th>
                             <th></th>
-                        </tr>                         
+                        </tr>
                 </table>
          )
     }
 }
- 
+
 export default StudentEnrolledTable;
